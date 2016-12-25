5e16c4f30cdb51f1c981b426580bc90325ccfca1
gaigai <gracegaiyue@gmail.com>
2016-12-25 19:59:44 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
    @group = Group.new(group_params)
    @group.user = current_user
-   
+
    if @group.save
       redirect_to groups_path
     else
@@ -21,6 +21,9 @@ end
 
  def edit
  @group = Group.find(params[:id])
+ 
+ if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
 
    if @group.save
      redirect_to groups_path
